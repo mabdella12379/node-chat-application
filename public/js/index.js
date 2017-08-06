a5232fb6a6731329ae350f9b95d37fac31935f9d
Mohamed Abdellatif <Mohamed Abdellatif>
2017-08-06 09:16:14 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -4,22 +4,8 @@ socket.on('connect', function() {
     console.log('Connected to server');
 });
 
-// socket.emit('createEmail', {
-//     to: 'jen@example.com',
-//     text: 'Hey. This is Andrew'
-// });
-
-// socket.on('newEmail', function(email) {
-//     console.log('New e-mail', email);
-// });
-
 socket.on('newMessage', function(message) {
     console.log('New message', message)
-});
-
-socket.emit('createMessage', {
-    from: 'kevin',
-    text: 'sure, I will'
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
-const path = require('path');
-const publicPath = path.join(__dirname, '/../public');
-// console.log(__dirname + '/../public');
-// console.log(publicPath);
 const express = require('express');
-const port = process.env.PORT || 3000;
 const socketIO = require('socket.io');
 const http = require('http');
+const path = require('path');
 
+const publicPath = path.join(__dirname, '/../public');
+const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app); 
 var io = socketIO(server);
@@ -15,25 +13,15 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
-
-    // socket.emit('newEmail', {
-    //     from: 'mike@example.com',
-    //     text: 'Hey, What is going on',
-    //     createAt: 123
-    // });
-
-    // socket.on('createEmail', (email) => {
-    //     console.log('createEmail', email)
-    // });
-    socket.emit('newMessage', {
-        from: 'todd',
-        text: 'Can you call me later?',
-        createdAt: 999
-    });
-
+    
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-    })
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
+        });
+    });
     
     socket.on('disconnect', () => {
         console.log('User was disconnected');
